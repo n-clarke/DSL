93dd492d42319f36633346ca1fec5acd387ba6ff
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-11-08 12:21:09 +0000

    added class

--- src/com/computer/CPU.kt
+++
@@ -1,7 +0,0 @@
-package com.computer
-
-class CPU {
-    private var values = mutableListOf<String>()
-    fun syrup(syrup:String) = values.add(syrup)
-    fun print() = values.map { "\t$it" }.forEach(::println)
-}
--- src/com/computer/DefaultPrograms.kt
+++
@@ -1,7 +0,0 @@
-package com.computer
-
-class DefaultPrograms   {
-    private var values = mutableListOf<String>()
-    operator fun String.unaryPlus() = values.add(this)
-    fun print() = values.map { "\t$it" }.forEach(::println)
-}
